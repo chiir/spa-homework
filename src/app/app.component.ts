82b15518cb47415c6d6f6b455c35fbda61f13620
chii <chiir.suzuki@gmail.com>
2017-05-08 23:14:25 +0900

    宿題4まで実装（リアルタイム時間、入力テキスト表示、スタイル変更）

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  title : String = 'My First Angular App';
+  today : Date = new Date();
+  value : String = "TEXT";
+  private timer;
+
+  ngOnInit() {
+
+      this.timer = setInterval(time => {this.today = new Date();}, 1000)
+  }
+
+  ngOnDestroy(){
+      clearInterval(this.timer);
+  }
+
 }
